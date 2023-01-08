1fadca89024396d450f606ac0c67897f8ea849b6
Mohit Goyal <76493813+mohitgithub2002@users.noreply.github.com>
2023-01-08 23:36:56 +0530

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 const url =
-  "mongodb+srv://rahul:rahul@clusterfords.yrejvhd.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://Saiyam1999:sam1999@cluster0.2l0ouiw.mongodb.net/?retryWrites=true&w=majority";
 export default url;
